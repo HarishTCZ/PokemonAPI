2d7027170abc8903fad3da847610f0e4a6d0384a
HarishTCZ <kumardeepaktc@gmail.com>
2024-12-10 23:37:37 +0530

    init

--- src/main/java/com/pokemon/Pokemon/Controller/PokemonController.java
+++ src/main/java/com/pokemon/Pokemon/Controller/PokemonController.java
@@ -3,6 +3,7 @@ package com.pokemon.Pokemon.Controller;
 import com.pokemon.Pokemon.Model.Pokemon;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,12 @@ public class PokemonController {
         pokemon.add(new Pokemon(2,"charmander","fire"));
         pokemon.add(new Pokemon(3,"squirtle","water"));
         return ResponseEntity.ok(pokemon);
+        //ResponseEntity.ok(pokemon) is shorthand for returning a response with a 200 status and the pokemon list as the body.
+    }
+
+    @GetMapping("/pokemon/{id}")
+    public Pokemon getId(@PathVariable int id){
+        //pathvariable is used to get the input data from the url path
+        return new Pokemon(id,"name","type");
     }
 }
